03100f6cd0f01d46cc871d591b47306b847fa3a1
rithin-rajpoot <rithinrajpoot@gmail.com>
2025-05-27 12:51:19 +0530

    Height issue

--- client/src/pages/home/MessageContainer.jsx
+++ client/src/pages/home/MessageContainer.jsx
@@ -50,7 +50,7 @@ const MessageContainer = () => {
       {!selectedUser ? (
         <NoChatSelected />
       ) : (
-        <div className="w-full flex flex-col">
+        <div className="w-full h-screen flex flex-col">
           <div className="px-2 py-2 border-b border-b-primary/30">
             <TopContainer userDetails={selectedUser} />
           </div>
