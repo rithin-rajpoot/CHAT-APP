d541b2865e566d09e6cb78cd2339d3ef58618c77
rithin-rajpoot <rithinrajpoot@gmail.com>
2025-05-27 13:01:40 +0530

    Modified the MessageContainer component to use the new message format

--- client/src/pages/home/MessageContainer.jsx
+++ client/src/pages/home/MessageContainer.jsx
@@ -32,7 +32,7 @@ const MessageContainer = () => {
         <NoChatSelected />
       ) : (
         <div className="w-full h-[100dvh] flex flex-col">
-          <div className="px-2 py-2 border-b border-b-primary/30">
+          <div className="px-2 py-2 border-b border-b-primary/30 sticky top-0 bg-base-100 z-10">
             <TopContainer userDetails={selectedUser} />
           </div>
           <div
