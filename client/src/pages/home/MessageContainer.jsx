db0dc9e494b1218c989cc23c6e19cd48fb00ea1c
rithin-rajpoot <rithinrajpoot@gmail.com>
2025-05-27 12:43:16 +0530

    Add: heigth: 100px;

--- client/src/pages/home/MessageContainer.jsx
+++ client/src/pages/home/MessageContainer.jsx
@@ -50,7 +50,7 @@ const MessageContainer = () => {
           <div className="px-2 py-2 border-b border-b-primary/30">
             <TopContainer userDetails={selectedUser} />
           </div>
-          <div className="h-full overflow-y-auto p-3" style={{ height: windowHeight }}>
+          <div className="overflow-y-auto p-3" style={{ height: windowHeight }}>
             {messages?.length === 0 ? (
               <NoMessages />
             ) : (
