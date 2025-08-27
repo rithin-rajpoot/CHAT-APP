17e5850c4a62c29577ed2a4e62f05f374f1a4041
rithin-rajpoot <rithinrajpoot@gmail.com>
2025-08-27 18:17:57 +0530

    Resolved bugs

--- client/src/pages/home/MessageContainer.jsx
+++ client/src/pages/home/MessageContainer.jsx
@@ -253,12 +253,12 @@ const MessageContainer = () => {
         <NoChatSelected />
       ) : (
         <div className="w-full h-full flex flex-col">
-          <div className="px-2 py-2 border-b border-b-primary/30 bg-base-100 z-10">
+          <div className="px-2 py-2 border-b border-b-primary/30 sticky top-0 bg-base-100 z-10">
             <TopContainer userDetails={selectedUser} />
           </div>
           <div
             ref={messagesContainerRef}
-            className={`overflow-y-auto p-3 transition-all duration-300 ease-in-out relative h-full ${
+            className={`overflow-y-auto p-3 transition-all duration-300 ease-in-out relative flex-1 ${
               isTyping ? "pb-6" : "pb-3"
             }`}
           >
