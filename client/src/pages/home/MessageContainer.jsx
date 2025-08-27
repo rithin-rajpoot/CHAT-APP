785f2838102e46b50cea3d3d0d374b36573d2395
rithin-rajpoot <rithinrajpoot@gmail.com>
2025-08-27 18:07:59 +0530

    Updated height of message container

--- client/src/pages/home/MessageContainer.jsx
+++ client/src/pages/home/MessageContainer.jsx
@@ -253,7 +253,7 @@ const MessageContainer = () => {
         <NoChatSelected />
       ) : (
         <div className="w-full h-full flex flex-col">
-          <div className="px-2 py-2 border-b border-b-primary/30 sticky top-0 bg-base-100 z-10">
+          <div className="px-2 py-2 border-b border-b-primary/30 fixed top-0 bg-base-100 z-10">
             <TopContainer userDetails={selectedUser} />
           </div>
           <div
